c7d1a4e3140c17e71942211f1e4cc8b8d7bb0797
Jordan <jordanmcrae@hotmail.com>
2015-04-02 14:01:44 -0400

    Exercise 6 completed, intentional error explained at the bottom in comments

--- object_oriented_programming/people.rb
+++ object_oriented_programming/people.rb
@@ -33,3 +33,11 @@ student_1 = Student.new("Cristina")
 
 puts instructor_1
 puts student_1
+
+puts instructor_1.teach
+puts student_1.teach
+
+# => Since .teach is part of the Instructor class, it cannot be called to the Student class,
+# => therefore puts student_1.teach returns an error. If the teach method were in the parent Person
+# => class, it could be called to both Instructor and Student. In this case, the output for the Student
+# => class should be puts student_1.learn
