b50ec5637a06cab4a4244fcadb4f6814cc92256b
Jordan <jordanmcrae@hotmail.com>
2015-04-02 13:52:53 -0400

    Created Person class and greeting -- steps 4 and 5

--- object_oriented_programming/people.rb
+++ object_oriented_programming/people.rb
@@ -1,12 +1,33 @@
-class Student
+class Person
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def to_s
+    "Hi, my name is #{@name}!"
+  end
+
+end
+
+
+class Student < Person
+
   def learn
     puts "I get it!"
   end
+
 end
 
-class Instructor
+class Instructor < Person
+
   def teach
     puts "Everything in Ruby is an object!"
   end
+
 end
 
+student_1 = Student.new("Frank")
+
+puts student_1
