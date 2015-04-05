45fd67f58f19231b931001aed4341541341154cc
Jordan <jordanmcrae@hotmail.com>
2015-04-05 01:54:31 -0400

    Plateau class progress - Unfinished

--- object_oriented_programming/rover/rover.rb
+++ object_oriented_programming/rover/rover.rb
@@ -72,49 +72,60 @@ class Plateau
 
   def grid_size
     puts "Please input the width of the grid (min. 1, max 9)."
-    x_dim = gets.chomp.to_i
-    if x_dim >= 10 or x_dim <= 0
+    @@x_dim = gets.chomp.to_i
+    if @@x_dim >= 10 or @@x_dim <= 0
       puts "Please select dimensions that are less than ten and greater than zero."
     else
-      @new_grid << x_dim
-      puts "Width of the grid has been set to #{x_dim}."
+      @new_grid << @@x_dim
+      puts "Width of the grid has been set to #{@@x_dim}."
     end
 
     puts "Please input the height of the grid (min. 1, max 9)."
-    y_dim = gets.chomp.to_i
-    if y_dim >= 10 or y_dim <= 0
+    @@y_dim = gets.chomp.to_i
+    if @@y_dim >= 10 or @@y_dim <= 0
       puts "Please select dimensions that are less than ten and greater than zero."
     else
-      @new_grid << y_dim
-      puts "Height of the grid has been set to #{y_dim}."
+      @new_grid << @@y_dim
+      puts "Height of the grid has been set to #{@@y_dim}."
     end
   end
 
   def add_a_rover
+
     puts "Please select a starting x-coordinate for the first rover."
     start_x = gets.chomp.to_i
+      if start_x > @@x_dim or start_x < 0
+        puts "Error. You cannot place the rover outside of the grid."
+      else
       @start_coord << start_x
       puts "The rover's x-coordinate has been set to #{start_x}."
+      end
 
     puts "Please select a starting y-coordinate for the first rover."
     start_y = gets.chomp.to_i
+    if start_y > @@y_dim or start_y < 0
+        puts "Error. You cannot place the rover outside of the grid."
+    else
       @start_coord << start_y
       puts "The rover's y-coordinate has been set to #{start_y}."
+    end
 
     puts "Please select a starting direction using N for North, S for South, E for East, and W for West."
     user_direction = gets.chomp.upcase.to_s
-    if user_direction != "N" or user_direction != "S" or user_direction != "E" or user_direction != "W"
-      puts "Error. Unknown direction command."
-    else
-      @start_coord << user_direction
-      puts "The direction of the new rover has been set to: #{user_direction}."
-    end
+    @start_coord << user_direction
+    puts "The direction of the new rover has been set to: #{user_direction}."
+
     puts
     "A new rover has been successfully created."
   end
 
   def move_rover
     puts
+  end
+
+  def instance_check
+    puts "new_grid = #{@new_grid}, start_coord = #{@start_coord}"
+  end
 end
 
 plat = Plateau.new
@@ -123,7 +134,7 @@ puts plat.grid_size
 
 puts plat.add_a_rover
 
-puts plat
+puts plat.instance_check
 
 
 
