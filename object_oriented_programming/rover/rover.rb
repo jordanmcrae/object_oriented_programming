6eecf67fcc0d21c26de6559ea93b82fa8023a3d0
Jordan <jordanmcrae@hotmail.com>
2015-04-05 17:18:59 -0400

    Working on move method

--- object_oriented_programming/rover/rover.rb
+++ object_oriented_programming/rover/rover.rb
@@ -60,14 +60,15 @@ class Rover
   end
 end
 
-class Plateau
-  attr_accessor :x_dim, :y_dim
+class Plateau < Rover
+  attr_accessor :x_dim, :y_dim, :new_grid, :start_coord, :start_coord2
 
   def initialize
     @x_dim = x_dim
     @y_dim = y_dim
     @new_grid = []
-    @start_coord = []
+    @@start_coord = []
+    @@start_coord2 = []
   end
 
   def grid_size
@@ -94,37 +95,90 @@ class Plateau
 
     puts "Please select a starting x-coordinate for the first rover."
     start_x = gets.chomp.to_i
-      if start_x > @@x_dim or start_x < 0
+    if start_x > @@x_dim or start_x < 0
         puts "Error. You cannot place the rover outside of the grid."
-      else
-      @start_coord << start_x
+    else
+      @@start_coord << start_x
       puts "The rover's x-coordinate has been set to #{start_x}."
-      end
+    end
 
     puts "Please select a starting y-coordinate for the first rover."
     start_y = gets.chomp.to_i
     if start_y > @@y_dim or start_y < 0
         puts "Error. You cannot place the rover outside of the grid."
     else
-      @start_coord << start_y
+      @@start_coord << start_y
       puts "The rover's y-coordinate has been set to #{start_y}."
     end
 
     puts "Please select a starting direction using N for North, S for South, E for East, and W for West."
     user_direction = gets.chomp.upcase.to_s
-    @start_coord << user_direction
+    @@start_coord << user_direction
     puts "The direction of the new rover has been set to: #{user_direction}."
 
     puts
     "A new rover has been successfully created."
   end
 
-  def move_rover
+  def additional_rover
+
+    puts "Please select a starting x-coordinate for the next rover."
+    start_x = gets.chomp.to_i
+
+    if start_x > @@x_dim or start_x < 0
+        puts "Error. You cannot place the rover outside of the grid."
+    else
+      @@start_coord2 << start_x
+      puts "The rover's x-coordinate has been set to #{start_x}."
+    end
+
+    puts "Please select a starting y-coordinate for the next rover."
+    start_y = gets.chomp.to_i
+
+    if start_y > @@y_dim or start_y < 0
+        puts "Error. You cannot place the rover outside of the grid."
+    else
+      @@start_coord2 << start_y
+      puts "The rover's y-coordinate has been set to #{start_y}."
+    end
+
+    puts "Please select a starting direction using N for North, S for South, E for East, and W for West."
+    user_direction = gets.chomp.upcase.to_s
+    @@start_coord2 << user_direction
+    puts "The direction of the new rover has been set to: #{user_direction}."
+
     puts
+    "A new rover has been successfully created."
+  end
+
+  def move
+    puts "Please select a rover by typing 1 for rover 1 which is located at #{@@start_coord} or 2 for rover 2 which is located at #{@@start_coord2}."
+    rover_select = gets.chomp.to_i
+
+    if rover_select == 1
+      puts "The first rover has been selected for movement."
+      puts "Please input commands to move the rover by typing M for move forward, L for turn left, and R for turn right. (E.g. MMMRMMLM)."
+      command_prompt = gets.chomp.
+      @@start_coord.read_instructions(command_prompt)
+      puts "The rover is now located at #{@@start_coord}"
+    elsif rover_select == 2
+        puts "The second rover has been selected for movement."
+        puts "Please input commands to move the rover by typing M for move forward, L for turn left, and R for turn right. (E.g. MMMRMMLM)."
+        command_prompt = gets.chomp
+        @@start_coord2.read_instructions(command_prompt)
+        puts "The rover is now located at #{@@start_coord2}"
+    else
+        puts "You have not selected a valid rover."
+    end
+  end
+
+
+  def to_s
+    "The rovers are located at: #{@start_coord.map{|a| a.to_s}} and #{@start_coord2.map{|b| b.to_s}}."
   end
 
   def instance_check
-    puts "new_grid = #{@new_grid}, start_coord = #{@start_coord}"
+    puts "new_grid = #{@new_grid}, start_coord = #{@@start_coord}, start_coord2 = #{@@start_coord2}"
   end
 end
 
@@ -134,7 +188,11 @@ puts plat.grid_size
 
 puts plat.add_a_rover
 
-puts plat.instance_check
+puts plat.additional_rover
+
+puts plat.move
+
+
 
 
 
