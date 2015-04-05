d4729e69b604dbba332e93115ce6fa3b885ab262
Jordan <jordanmcrae@hotmail.com>
2015-04-04 21:19:25 -0400

    Beginning of plateau class.

--- object_oriented_programming/rover/rover.rb
+++ object_oriented_programming/rover/rover.rb
@@ -56,42 +56,76 @@ class Rover
   end
 
   def to_s  #Reaches into state, pulls it out, and formats it into a nice string for printing. Allows us to do puts for my_rover.
-    "I am at #{@x}, #{@y}, facing #{@direction}."
+    "I am at #{@x}, #{@y}, facing #{@direction}. "
   end
 end
-# ----------------------------
+
 class Plateau
+  attr_accessor :x_dim, :y_dim
 
-  def initialize (length, width)
-    @length = length
-    @width = width
-    @rover_list = []      # => Going to create an array of the rover locations
+  def initialize
+    @x_dim = x_dim
+    @y_dim = y_dim
+    @new_grid = []
+    @start_coord = []
   end
 
-    def new_rover(x, y, direction)
-      if x > @width or x < 0 || y > @length or y < 0
-        puts "Please make sure to place the rover on the Plateau."
-      else
-        add_rover = Rover.new(x, y, direction)
-        @rover_list << add_rover        # => Shovel the new rover into the rover_list array to be accessed later
-      end
-    end
-    def move_rover
-      puts "Please enter commands to move the rover. L = turn left, R = turn right, M = move forward (syntax e.g. /'RMMMMLMLM'/)."
-      movement = gets.chomp
-      @rover_list[].read_instructions(movement)
+  def grid_size
+    puts "Please input the width of the grid (min. 1, max 9)."
+    x_dim = gets.chomp.to_i
+    if x_dim >= 10 or x_dim <= 0
+      puts "Please select dimensions that are less than ten and greater than zero."
+    else
+      @new_grid << x_dim
+      puts "Width of the grid has been set to #{x_dim}."
     end
 
-    def to_s
-    "The dimensions of the grid are: #{@width} units high by #{@length} units wide. #{@rover_list.collect{|x| x}}"
+    puts "Please input the height of the grid (min. 1, max 9)."
+    y_dim = gets.chomp.to_i
+    if y_dim >= 10 or y_dim <= 0
+      puts "Please select dimensions that are less than ten and greater than zero."
+    else
+      @new_grid << y_dim
+      puts "Height of the grid has been set to #{y_dim}."
     end
+  end
+
+  def add_a_rover
+    puts "Please select a starting x-coordinate for the first rover."
+    start_x = gets.chomp.to_i
+      @start_coord << start_x
+      puts "The rover's x-coordinate has been set to #{start_x}."
+
+    puts "Please select a starting y-coordinate for the first rover."
+    start_y = gets.chomp.to_i
+      @start_coord << start_y
+      puts "The rover's y-coordinate has been set to #{start_y}."
+
+    puts "Please select a starting direction using N for North, S for South, E for East, and W for West."
+    user_direction = gets.chomp.upcase.to_s
+    if user_direction != "N" or user_direction != "S" or user_direction != "E" or user_direction != "W"
+      puts "Error. Unknown direction command."
+    else
+      @start_coord << user_direction
+      puts "The direction of the new rover has been set to: #{user_direction}."
+    end
+    puts
+    "A new rover has been successfully created."
+  end
+
+  def move_rover
+    puts
 end
 
+plat = Plateau.new
+
+puts plat.grid_size
+
+puts plat.add_a_rover
+
+puts plat
 
 
 
-plateau = Plateau.new(9,9)
 
-plateau.new_rover(1, 1, "E")
-plateau.new_rover(2, 2, "N")
-puts plateau
+
